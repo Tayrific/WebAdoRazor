d63d704bacfbf0849b23e601cb2d46c0f045d118
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-06-07 10:36:41 +0100

    Add project files.

---
+++ WebAdoRazor/Pages/Index.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace WebAdoRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public readonly IConfiguration _configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public List<Customer> Customers { get; set; }
+        public void OnGet()
+        {
+            Customers = new List<Customer>(); 
+
+            string con = _configuration.GetSection("ConnectionStrings")["Northwind"];
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+
+                string sqlQuery = "SELECT * FROM Customers";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Customers.Add(new Customer
+                            {
+                                CustomerID = reader["CustomerID"].ToString(),
+                                CompanyName = reader["CompanyName"].ToString(),
+                                ContactName = reader["ContactName"].ToString(),
+                                ContactTitle = reader["ContactTitle"].ToString(),
+                                Address = reader["Address"].ToString(),
+                                City = reader["City"].ToString(),
+                                Region = reader["Region"].ToString(),
+                                PostalCode = reader["PostalCode"].ToString(),
+                                Country = reader["Country"].ToString(),
+                                Phone = reader["Phone"].ToString(),
+                                Fax = reader["Fax"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+    }
+}
